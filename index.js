e3010cbca001823c9a4bcc1b2ff60198cbef9e0d
Sone <sthaya@gmail.com>
2020-10-16 20:48:11 -0700

    adjusted port now pulling data from database

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
-const Post = require('./models/Post');
+const Post = require("./models/Post");
 const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    getPosts
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello World!",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
@@ -25,7 +38,7 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    return server.listen({ port: 5000 });
+    return server.listen({ port: 8000 });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
